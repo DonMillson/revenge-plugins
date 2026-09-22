6b37baa0a1c8874c7915d7e20704270550d6f8ce
DonMillson <donmillson@gmail.com>
2026-09-22 12:46:35 +0200

    Fix FakeNitro for Revenge Classic bundle format

--- fake-nitro/index.js
+++ fake-nitro/index.js
@@ -1,44 +1,94 @@
-const { findByProps, findByStoreName } = vendetta.metro;
-const { instead, before } = vendetta.patcher;
-const { storage } = vendetta.plugin;
+(function(exports, metro, common, api, plugin) {
+"use strict";
 
+const storage = plugin.storage;
 storage.emojiSize ??= 48;
 storage.forceEmoji ??= false;
 
-const unpatches = [];
-const nitroInfo = findByProps("canUseEmojisEverywhere");
-if (nitroInfo) {
-  if (nitroInfo.canUseEmojisEverywhere) unpatches.push(instead("canUseEmojisEverywhere", nitroInfo, () => true));
-  if (nitroInfo.canUseAnimatedEmojis) unpatches.push(instead("canUseAnimatedEmojis", nitroInfo, () => true));
+let unpatches = [];
+
+function safeFindByProps(...props) {
+  try { return metro.findByProps?.(...props) || metro.findByPropsLazy?.(...props); }
+  catch { return null; }
 }
 
-const EmojiStore = findByStoreName("EmojiStore");
-const SelectedGuildStore = findByStoreName("SelectedGuildStore");
-const UserStore = findByStoreName("UserStore");
+function safeStore(name) {
+  try { return metro.findByStoreName?.(name) || metro.findByStoreNameLazy?.(name); }
+  catch { return null; }
+}
 
 function modifyIfNeeded(msg) {
-  if (!msg || typeof msg.content !== "string") return;
-  if (!/<a?:\w+:\d+>/i.test(msg.content)) return;
+  try {
+    if (!msg || typeof msg.content !== "string") return;
+    if (!/<a?:\\w+:\\d+>/i.test(msg.content)) return;
 
-  const premiumType = UserStore?.getCurrentUser?.()?.premiumType;
-  if (!storage.forceEmoji && premiumType != null && premiumType !== 0) return;
+    const EmojiStore = safeStore("EmojiStore");
+    const SelectedGuildStore = safeStore("SelectedGuildStore");
+    const UserStore = safeStore("UserStore");
 
-  const currentGuild = SelectedGuildStore?.getGuildId?.();
-  msg.content = msg.content.replace(/<a?:(\w+):(\d+)>/gi, (full, name, id) => {
-    const emoji = EmojiStore?.getCustomEmojiById?.(id);
-    if (!emoji) return full;
-    const usableHere = emoji.guildId === currentGuild && !emoji.animated;
-    if (usableHere && !storage.forceEmoji) return full;
-    const animated = emoji.animated ? "&animated=true" : "";
-    return `[${name}](https://cdn.discordapp.com/emojis/${id}.webp?size=${storage.emojiSize}&quality=lossless&name=${encodeURIComponent(name)}${animated})`;
-  });
-  msg.invalidEmojis = [];
+    const premiumType = UserStore?.getCurrentUser?.()?.premiumType;
+    if (!storage.forceEmoji && premiumType != null && premiumType !== 0) return;
+
+    const currentGuild = SelectedGuildStore?.getGuildId?.();
+
+    msg.content = msg.content.replace(/<a?:(\\w+):(\\d+)>/gi, (full, name, id) => {
+      const emoji = EmojiStore?.getCustomEmojiById?.(id);
+      if (!emoji) return full;
+
+      const usableHere = emoji.guildId === currentGuild && !emoji.animated;
+      if (usableHere && !storage.forceEmoji) return full;
+
+      const animated = emoji.animated ? "&animated=true" : "";
+      return `[${name}](https://cdn.discordapp.com/emojis/${id}.webp?size=${Number(storage.emojiSize) || 48}&quality=lossless&name=${encodeURIComponent(name)}${animated})`;
+    });
+
+    msg.invalidEmojis = [];
+  } catch {}
 }
 
-const messageModule = findByProps("sendMessage", "receiveMessage");
-if (messageModule?.sendMessage) unpatches.push(before("sendMessage", messageModule, args => modifyIfNeeded(args[1])));
+function onLoad() {
+  const patcher = api.patcher || api;
+  const before = patcher?.before;
+  const instead = patcher?.instead;
 
-const uploadModule = findByProps("uploadLocalFiles");
-if (uploadModule?.uploadLocalFiles) unpatches.push(before("uploadLocalFiles", uploadModule, args => modifyIfNeeded(args[0]?.parsedMessage)));
+  try {
+    const nitroInfo = safeFindByProps("canUseEmojisEverywhere");
+    if (nitroInfo && typeof instead === "function") {
+      if (typeof nitroInfo.canUseEmojisEverywhere === "function")
+        unpatches.push(instead("canUseEmojisEverywhere", nitroInfo, () => true));
+      if (typeof nitroInfo.canUseAnimatedEmojis === "function")
+        unpatches.push(instead("canUseAnimatedEmojis", nitroInfo, () => true));
+    }
+  } catch {}
 
-export const onUnload = () => unpatches.forEach(fn => { try { fn(); } catch {} });
+  try {
+    const messageModule = safeFindByProps("sendMessage", "receiveMessage");
+    if (messageModule?.sendMessage && typeof before === "function")
+      unpatches.push(before("sendMessage", messageModule, args => modifyIfNeeded(args?.[1])));
+  } catch {}
+
+  try {
+    const uploadModule = safeFindByProps("uploadLocalFiles");
+    if (uploadModule?.uploadLocalFiles && typeof before === "function")
+      unpatches.push(before("uploadLocalFiles", uploadModule, args => modifyIfNeeded(args?.[0]?.parsedMessage)));
+  } catch {}
+}
+
+function onUnload() {
+  for (const unpatch of unpatches) {
+    try { unpatch?.(); } catch {}
+  }
+  unpatches = [];
+}
+
+const index = { onLoad, onUnload };
+exports.default = index;
+Object.defineProperty(exports, "__esModule", { value: true });
+return exports;
+})(
+  {},
+  typeof bunny !== "undefined" && bunny.metro ? bunny.metro : vendetta.metro,
+  typeof bunny !== "undefined" && bunny.metro?.common ? bunny.metro.common : vendetta.metro.common,
+  typeof bunny !== "undefined" && bunny.api?.patcher ? bunny.api : vendetta,
+  vendetta.plugin
+);
